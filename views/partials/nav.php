8967d8da8c6db25fe2b0d04b1f47f5189025f513
Anjitha-94 <122707518+Anjitha-94@users.noreply.github.com>
2023-10-26 15:18:44 -0700

    Add the content of navigation to nav file

---
+++ views/partials/nav.php
@@ -0,0 +1,13 @@
+<body>
+    <header>
+        <h1>Doctor Booking System</h1>
+    </header>
+    <section class="navWrap">
+    <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="doctors.html">Doctors</a></li>
+            <li><a href="appointments.html">Appointments</a></li>
+        </ul>
+    </nav>
+    </section>
