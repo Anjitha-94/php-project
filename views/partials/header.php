eb066336873c23be408cc36489859cc25fb7ae71
Anjitha-94 <122707518+Anjitha-94@users.noreply.github.com>
2023-11-04 12:42:20 -0700

    Add images to the project

--- views/partials/header.php
+++ views/partials/header.php
@@ -12,9 +12,9 @@
 <section class="navWrap">
     <nav>
         <ul>
-            <li><a href="controllers/index.php">Home</a></li>
-            <li><a href="controllers/doctors.php">Doctors</a></li>
-            <li><a href="controllers/appointments.php">Appointments</a></li>
+            <li><a href="index.view.php">Home</a></li>
+            <li><a href="views/doctors.view.php">Doctors</a></li>
+            <li><a href="views/appointments.view.php">Appointments</a></li>
         </ul>
     </nav>
     </section>
