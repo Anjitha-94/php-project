5f118937c3ffe6463a1c630f07c6d357a2d90861
Anjitha-94 <122707518+Anjitha-94@users.noreply.github.com>
2023-11-05 16:14:49 -0800

    Add database file to the project

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'database'=>[
+    'host' => 'localhost',
+    'port' => 3306,
+    'dbname' => 'myproject',
+    'charset' => 'utf8mb4'
+    ],
+
+];
--- index.php
+++ index.php
@@ -2,3 +2,17 @@
 
 require 'router.php';
 
+require 'Database.php';
+
+$config = require ('config.php');
+
+$db = new Database($config['database']);
+
+$id = $_GET['id'];
+$query = "select * from posts where id = :id";
+
+
+$posts = $db->query($query, [':id'=>$id])->fetch();
+
+dd($posts);
+
