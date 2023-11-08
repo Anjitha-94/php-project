86da2d2d9d4be8ee0afdb02321c30277f687348b
Anjitha-94 <122707518+Anjitha-94@users.noreply.github.com>
2023-11-07 18:57:47 -0800

    Fetch the clinic list from the database

--- views/doctors.view.php
+++ views/doctors.view.php
@@ -1,4 +1,5 @@
 <?php require('partials/header.php')?>
+<?php require('../process.cliniclist.php')?>
 
 
              <section class="list">
@@ -6,45 +7,14 @@
                 <h1>List of Clinic Details</h1>
                 <div class="clinic">
                     <?php foreach ($clinics as $clinic):?>
-                    <a href="clinic-details.view.php" class="clinic-name">Clinic 1</a>
-                    <p><strong>Address:</strong> 123 Main St, Courtenay, BC ZIP</p>
-                    <p><strong>Phone:</strong> (555) 123-4567</p>
-                    <p><strong>Email:</strong> <a href="mailto:clinic1@example.com">clinic1@example.com</a></p>
+                    <a href="clinic-details.view.php" class="clinic-name"><?= $clinic['Name'] ?></a>
+                    <p><strong>Address:</strong><?= $clinic['Address'] ?></p>
+                    <p><strong>Phone:</strong> <?= $clinic['phone'] ?></p>
+                    <p><strong>Email:</strong> <a href="mailto:<?= $clinic['email'] ?>"><?= $clinic['email'] ?></a></p>
                     <?php endforeach; ?>
                 </div>
             
-                <div class="clinic">
-                <?php foreach ($clinics as $clinic):?>
-                    <a href="clinic-details.view.php" class="clinic-name">Clinic 2</a>
-                    <p><strong>Address:</strong> 456 Elm St, Comox, BC ZIP</p>
-                    <p><strong>Phone:</strong> (555) 987-6543</p>
-                    <p><strong>Email:</strong> <a href="mailto:clinic2@example.com">clinic2@example.com</a></p>
-                    <?php endforeach; ?>
-                </div>
-                <div class="clinic">
-                <?php foreach ($clinics as $clinic):?>
-                    <a href="clinic-details.view.php" class="clinic-name">Clinic 3</a>
-                    <p><strong>Address:</strong> 145 Main St, Courtenay, BC ZIP</p>
-                    <p><strong>Phone:</strong> (555) 123-4789</p>
-                    <p><strong>Email:</strong> <a href="mailto:clinic1@example.com">clinic1@example.com</a></p>
-                    <?php endforeach; ?>
-                </div>
-                <div class="clinic">
-                <?php foreach ($clinics as $clinic):?>
-                    <a href="clinic-details.view.php" class="clinic-name">Clinic 4</a>
-                    <p><strong>Address:</strong> 789 Main St, Comox, BC ZIP</p>
-                    <p><strong>Phone:</strong> (555) 189-4521</p>
-                    <p><strong>Email:</strong> <a href="mailto:clinic1@example.com">clinic1@example.com</a></p>
-                    <?php endforeach; ?>
-                </div>
-            </section>
-            <div class="button-container">
-                <a href="index.view.php" class="btn">Back</a>   
-              </div>
-              
-           
-    
-                <!-- Add more clinics as needed -->
+            
            
   
     <?php require('partials/footer.php')?>
